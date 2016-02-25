0afd7c378791b8a64c31215c9e1d49782a3f568a
Peter Mamonov <pmamonov@gmail.com>
2016-02-25 23:08:41 +0300

    fixup! queue: add debug

--- src/uqueue.c
+++ src/uqueue.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include "uqueue.h"
 
+static int bug_report = 2;
+
 static int queue_lock(struct queue *q)
 {
 	int ret = -1;
@@ -37,12 +39,16 @@ void queue_init(struct queue *q, int sz, int cap, void *ptr)
 
 static inline void queue_check(struct queue *q, char *func)
 {
-	if (q->len != (q->in >= q->out ? q->in - q->out : q->in + q->cap - q->out))
-		printf("BUG: %s: in: %d out: %d len: %d\n\r",
+	if (bug_report &&
+	    q->len != (q->in >= q->out ? q->in - q->out : q->in + q->cap - q->out)) {
+		printf("BUG: %s: in=%d out=%d len=%d q=%p\n\r",
 			func,
 			q->in,
 			q->out,
-			q->len);
+			q->len,
+			q);
+		bug_report--;
+	}
 }
 
 int queue_push(struct queue *q, void *ptr)
@@ -53,10 +59,11 @@ int queue_push(struct queue *q, void *ptr)
 		queue_unlock(q);
 		return -1;
 	}
+	queue_check(q, "push-in");
 	memcpy(q->ptr + q->sz * q->in, ptr, q->sz);
 	q->in = (q->in + 1) % q->cap;
 	q->len += 1;
-	queue_check(q, __func__);
+	queue_check(q, "push-out");
 	queue_unlock(q);
 	return 0;
 }
@@ -69,10 +76,11 @@ int queue_pop(struct queue *q, void *ptr)
 		queue_unlock(q);
 		return -1;
 	}
+	queue_check(q, "pop-in");
 	memcpy(ptr, q->ptr + q->sz * q->out, q->sz);
 	q->out = (q->out + 1) % q->cap;
 	q->len -= 1;
-	queue_check(q, __func__);
+	queue_check(q, "pop-out");
 	queue_unlock(q);
 	return 0;
 }
@@ -88,11 +96,15 @@ int queue_swap(struct queue *q1, struct queue *q2)
 		taskENABLE_INTERRUPTS();
 		return -1;
 	}
+	queue_check(q1, "swap-in-q1");
+	queue_check(q2, "swap-in-q2");
 
 	memcpy(&tmp, q1, sizeof(*q1));
 	memcpy(q1, q2, sizeof(*q1));
 	memcpy(q2, &tmp, sizeof(*q1));
 
+	queue_check(q1, "swap-out-q1");
+	queue_check(q2, "swap-out-q2");
 	taskENABLE_INTERRUPTS();
 	return 0;
 }
